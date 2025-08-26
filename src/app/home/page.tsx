f7c1f9fe67951253160dd92ccc205ceebe4c3bdc
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-08-25 23:01:55 -0600

    first commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>This is Home page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is Home page</h1>
+    </div>
+  );
+}
