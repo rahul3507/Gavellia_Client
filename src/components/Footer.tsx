f31b0d6cea1cd245ca93e8cf5e42fbdbeaffa1a3
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-08-26 03:48:40 -0600

    Add new dependencies and update Footer component with icons and layout improvements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,166 +1,172 @@
 /** @format */
 
 import React from "react";
+import {
+  FaApple,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { FaX, FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className="bg-card-bg mt-16">
       <div className="w-full  px-8 sm:px-8 lg:px-16 xl:px-24 2xl:px-32 py-12">
-        <div className="grid grid-cols-2 md:grid-cols-6 gap-8">
-          <div>
-            <h3 className="font-semibold text-[#1c1c1c] mb-4">Marketplace</h3>
-            <ul className="space-y-2 text-sm text-[#1c1c1c]/70">
+        <div className="w-full grid  grid-cols-2 md:grid-cols-5 gap-8 ">
+          <div className="">
+            <h3 className="text-sm text-primary mb-4">Marketplace</h3>
+            <ul className="space-y-2 text-sm md:text-base text-primary">
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className=" hover:text-primary/70">
                   Live Auctions
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className=" hover:text-primary/70">
                   Shop Products
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className=" hover:text-primary/70">
                   Trending Deals
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className=" hover:text-primary/70">
                   Flash Sales
                 </a>
               </li>
             </ul>
           </div>
-          <div>
-            <h3 className="font-semibold text-[#1c1c1c] mb-4">Categories</h3>
-            <ul className="space-y-2 text-sm text-[#1c1c1c]/70">
+          <div className="">
+            <h3 className="text-sm text-primary mb-4">Categories</h3>
+            <ul className="space-y-2 text-sm md:text-base text-primary">
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Fashion & Apparel
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Electronics
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Home & Living
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Collectibles
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Luxury Items
                 </a>
               </li>
             </ul>
           </div>
-          <div>
-            <h3 className="font-semibold text-[#1c1c1c] mb-4">Support</h3>
-            <ul className="space-y-2 text-sm text-[#1c1c1c]/70">
+          <div className="">
+            <h3 className="text-sm text-primary mb-4">Support</h3>
+            <ul className="space-y-2 text-sm md:text-base text-primary">
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Help Center
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Shipping Policy
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Returns & Refunds
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Contact Support
                 </a>
               </li>
             </ul>
           </div>
-          <div>
-            <h3 className="font-semibold text-[#1c1c1c] mb-4">Company</h3>
-            <ul className="space-y-2 text-sm text-[#1c1c1c]/70">
+
+          <div className="">
+            <h3 className="text-sm text-primary mb-4">Company</h3>
+            <ul className="space-y-2 text-sm md:text-base text-primary">
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   About Us
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Career
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-[#79764f]">
+                <a href="#" className="hover:text-primary/70">
                   Blogs
                 </a>
               </li>
             </ul>
           </div>
-          <div>
-            <h3 className="font-semibold text-[#1c1c1c] mb-4">Company</h3>
-            <ul className="space-y-2 text-sm text-[#1c1c1c]/70">
-              <li>
-                <a href="#" className="hover:text-[#79764f]">
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-[#79764f]">
-                  Career
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-[#79764f]">
-                  Blogs
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="font-semibold text-[#1c1c1c] mb-4">
-              Download Our App
-            </h3>
+          <div className="">
+            <h3 className="text-sm text-primary mb-4">Download Our App</h3>
             <div className="space-y-2">
-              <img
-                src="/app-store-download-button.png"
-                alt="App Store"
-                className="h-10"
-              />
-              <img
-                src="/google-play-download-button.png"
-                alt="Google Play"
-                className="h-10"
-              />
+              <Button className="flex items-center space-x-2 border-none px-3 py-2 rounded cursor-pointer w-max bg-white hover:bg-white/70 text-primary">
+                <FaApple />
+                <div className="pr-2.5">
+                  <p className="text-[8px]">Download on the</p>
+                  <p className="text-sm">App Store</p>
+                </div>
+              </Button>
+              <Button className="flex items-center space-x-0 border-none px-3 py-2 rounded cursor-pointer w-max bg-white hover:bg-white/70 text-primary">
+                <Image
+                  src="/playstore.png"
+                  alt="Google Play"
+                  width={20}
+                  height={20}
+                />
+                <div>
+                  <p className="text-[8px]">GET IT ON</p>
+                  <p className="text-sm">Google Play</p>
+                </div>
+              </Button>
             </div>
           </div>
         </div>
 
-        <div className="border-t border-[#e9e9e9] mt-8 pt-8 flex justify-between items-center">
-          <div className="text-sm text-[#1c1c1c]/70">
+        <div className="border-t border-[#e9e9e9] mt-6 pt-4 flex justify-between items-center">
+          <div className="text-base text-primary/70 gap-8">
             © 2024 Vendra Inc.{" "}
-            <a href="#" className="hover:text-[#79764f]">
+            <a href="#" className="text-link hover:text-link ml-2 underline">
               Terms of Service
             </a>{" "}
-            <a href="#" className="hover:text-[#79764f]">
+            <a href="#" className="text-link hover:text-link ml-2 underline">
               Privacy Policy
             </a>
           </div>
           <div className="flex space-x-4">
-            <div className="w-6 h-6 bg-[#1877f2] rounded"></div>
-            <div className="w-6 h-6 bg-[#1c1c1c] rounded"></div>
-            <div className="w-6 h-6 bg-[#0a66c2] rounded"></div>
-            <div className="w-6 h-6 bg-[#ff0302] rounded"></div>
+            <Button className="w-6 h-6 bg-white text-[#1877f2] flex justify-center items-center">
+              <FaFacebook className="" />
+            </Button>
+            <Button className="w-6 h-6 bg-white text-[#1c1c1c] rounded flex justify-center items-center">
+              <FaXTwitter />
+            </Button>
+            <Button className="w-6 h-6 bg-white text-[#0a66c2] rounded flex justify-center items-center">
+              <FaLinkedin />
+            </Button>
+            <Button className="w-6 h-6 bg-white text-[#ff0302] rounded flex justify-center items-center">
+              <FaYoutube />
+            </Button>
           </div>
         </div>
       </div>
