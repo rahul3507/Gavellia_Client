d0d70cc7d4253351432b0712553afefc1f7cdf3c
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-10-18 11:42:07 +0600

    feat(ProductLive): add bid sheet component and display current bid information

--- src/app/products/[title]/live/page.tsx
+++ src/app/products/[title]/live/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import React, { useState, useEffect, use } from "react";
 import { Button } from "@/components/ui/button";
+import BidSheet from "@/components/productsComponents/bidSheet";
 
 import {
   Carousel,
@@ -293,6 +294,44 @@ const ProductLive: React.FC<ProductDetailsProps> = ({ params }) => {
                         New York, NY, United States
                       </p>
                     </div>
+                  </div>
+                </div>
+
+                <div className="w-full max-w-full overflow-hidden mt-8 space-y-3 mb-12">
+                  <div className="w-full flex justify-between border-b border-gray-200 pb-4">
+                    <div className="text-xs ">Lot 1/14</div>
+                    <div className="text-green-600 font-semibold text-xs">
+                      IN PROGRESS
+                    </div>
+                  </div>
+                  <div className="w-full flex  justify-between ">
+                    <div className="text-gray-600 text-xs">Current Bid</div>
+                    <div className="text-gray-900 text-base">0 Bids</div>
+                  </div>
+
+                  <div className="text-3xl font-semibold text-gray-800">
+                    N/A
+                  </div>
+
+                  <div className="text-gray-600 mt-4 text-base">
+                    The bid is not yours
+                  </div>
+                  <div className="flex space-x-4 mt-4">
+                    <BidSheet
+                      currentBid={Number(productData.highestBid)}
+                      minimumBid={300}
+                      productTitle={productData.title}
+                    >
+                      <Button className="bg-black text-white px-6 py-2 rounded-none flex-1">
+                        BID £300
+                      </Button>
+                    </BidSheet>
+                    <Button className="bg-gray-200 text-black px-6 py-2 rounded-none flex-1">
+                      SWITCH TO NEXT BID
+                    </Button>
+                  </div>
+                  <div className="text-gray-600 text-sm mt-2">
+                    All bids are binding.
                   </div>
                 </div>
               </div>
